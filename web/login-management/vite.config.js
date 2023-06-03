cc0804d837850a71a07ee84b8b03b8b4c3f6446a
MrMsj <msjstream@outlook.com>
2023-06-03 17:06:49 +0800

    test: add test to vite.config.js

--- web/login-management/vite.config.js
+++ web/login-management/vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,'./src')
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.2.117:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
-})
+});
