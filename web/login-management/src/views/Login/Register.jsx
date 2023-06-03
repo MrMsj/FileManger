5181a7a60ebcdbec0b264dc2ff963fb8496a8cc0
MrMsj <msjstream@outlook.com>
2023-06-03 15:04:49 +0800

    feat(front): edit redux

--- web/login-management/src/views/Login/Register.jsx
+++ web/login-management/src/views/Login/Register.jsx
@@ -18,9 +18,6 @@ const View= () =>{
   // 获取用户输入的信息
   const [usernameVal,setUsernameVal] = useState(""); // 定义用户输入用户名这个变量
   const [passwordVal,setPasswordVal] = useState(""); // 定义用户输入密码这个变量
-  const [captchaVal,setCaptchaVal] = useState(""); // 定义用户输入验证码这个变量
-  // 定义一个变量保存验证码图片信息
-  const [captchaImg,setCaptchaImg] = useState(""); 
 
   const usernameChange = (e)=>{
     // 获取用户输入的用户名
@@ -31,55 +28,23 @@ const View= () =>{
   const passwordChange = (e)=>{
     setPasswordVal(e.target.value);
   }
-  const captchaChange = (e)=>{
-    setCaptchaVal(e.target.value);
-  }
 
-  // 点击登录按钮的事件函数
-  const gotoLogin = async ()=>{
-    console.log("用户输入的用户名，密码，验证码分别是：",usernameVal,passwordVal,captchaVal);
-    // 验证是否有空值
-    if(!usernameVal.trim() || !passwordVal.trim()|| !captchaVal.trim()){
-      message.warning("请完整输入信息！")
+  const gotoRegister=async()=>{
+    if(!usernameVal.trim() || !passwordVal.trim()){
+      message.warning("请完整输入信息！");
       return
     }
-    // 发起登录请求
-    let loginAPIRes = await LoginAPI({
+    let registerAPIRes=await RegisterAPI({
       username:usernameVal,
       password:passwordVal,
-      code:captchaVal,   
-      uuid:localStorage.getItem("uuid")})
+      uuid:localStorage.getItem("uuid")
+    });
 
-      console.log(loginAPIRes);
-      if(loginAPIRes.code===200){
-        // 1、提示登录成功
-        message.success("登录成功！")
-        // 2、保存token
-        localStorage.setItem("lege-react-management-token",loginAPIRes.token)
-        // 3、跳转到/page1
-        navigateTo("/page1")
-        // 4、删除本地保存中的uuid
-        localStorage.removeItem("uuid")
-      }
-  }
-
-  const gotoRegister=()=>{
-    navigateTo("./Register.jsx");
-  }
-
-  // 点击验证码图片盒子的事件函数
-  const getCaptchaImg = async ()=>{
-    // 做验证码的请求
-    // CaptchaAPI().then((res)=>{
-    //   console.log(res);
-    // })
-    let captchaAPIRes = await CaptchaAPI();
-    console.log(captchaAPIRes);  
-    if(captchaAPIRes.code===200){
-      // 1、把图片数据显示在img上面
-      setCaptchaImg("data:image/gif;base64,"+captchaAPIRes.img)
-      // 2、本地保存uuid，给登录的时候用
-      localStorage.setItem("uuid",captchaAPIRes.uuid)
+    if(registerAPIRes.code==200){
+      message.success("注册成功！");
+      localStorage.setItem("login-react-management-token", registerAPIRes.token);
+      navigateTo("/page1");
+      localStorage.removeItem("uuid");
     }
   }
 
@@ -110,7 +75,6 @@ const View= () =>{
                 <Divider style={{borderColor:"#1890ff",color:"#1890ff"}} />
                 <div style={{display:"flex",justifyContent:"center"}}>
                   <Space size={100}>
-                    <Button type="primary" onClick={gotoLogin}>登录</Button>
                     <Button type="primary" onClick={gotoRegister}>注册</Button>
                   </Space>
                 </div>
